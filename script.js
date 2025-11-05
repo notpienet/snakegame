1058073cbcf0b6ad36964ea9d6c996cfad150d72
notpienet <115167754+notpienet@users.noreply.github.com>
2025-11-05 22:12:37 +0700

    speed changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const game = document.getElementById("game");
-let n = 9, m = 9;
+let n = 17, m = 17;
 let g1 = "gray", g2 = "lightgray";
 for(let i = 1; i <= n; i++) {
     const row = document.createElement("div");
@@ -19,10 +19,11 @@ for(let i = 1; i <= n; i++) {
     game.append(row);
 }
 
-const pos = [[Math.ceil(n / 2), Math.ceil(m / 2)]];
+const pos = [[Math.ceil(n / 2), Math.ceil(m / 2) - 2]];
 let x = pos[0][0], y = pos[0][1];
 let dir = 2, last = 2;
-let cur = 5;
+let cur = 30, spawn = 1;
+const apple = [];
 
 function update() {
     x = pos[pos.length - 1][0];
@@ -33,8 +34,6 @@ function update() {
     if(dir == 3) pos.push([x + 1, y]);
     if(dir == 4) pos.push([x, y - 1]);
     if(pos.length > cur) pos.shift();
-    x = pos[pos.length - 1][0];
-    y = pos[pos.length - 1][1];
     for(let i = 1; i <= n; i++) {
         for(let j = 1; j <= m; j++) {
             const img = document.getElementById(i.toString() + "." + j.toString());
@@ -47,16 +46,22 @@ function update() {
         if(pos[i + 1][0] == pos[i][0] - 1) b = 1;
         if(pos[i + 1][1] == pos[i][1] + 1) b = 2;
         if(pos[i + 1][1] == pos[i][1] - 1) b = 4;
-        console.log(pos[i][0].toString() + "_" + pos[i][1].toString());
-        const img = document.getElementById(pos[i][0].toString() + "." + pos[i][1].toString());
-        img.src = "assets/" + Math.min(a, b).toString() + Math.max(a, b).toString() + ".png";
-        const fade = Math.min(1, pos.length * 0.1) * i / (pos.length - 1);
-        img.style.filter = "brightness(" + (Math.min(2, 1 + pos.length * 0.1) - fade).toString() + ")";
+        if(pos[i][0] >= 1 && pos[i][0] <= n && pos[i][1] >= 1 && pos[i][1] <= m) {
+            console.log(pos[i][0].toString() + "_" + pos[i][1].toString());
+            const img = document.getElementById(pos[i][0].toString() + "." + pos[i][1].toString());
+            img.src = "assets/" + Math.min(a, b).toString() + Math.max(a, b).toString() + ".png";
+            const fade = Math.min(1, pos.length * 0.1) * i / (pos.length - 1);
+            img.style.filter = "brightness(" + (Math.min(2, 1 + pos.length * 0.1) - fade).toString() + ")";
+        }
         a = ((b + 1) % 4 + 1);
     }
-    const temp = document.getElementById(x.toString() + "." + y.toString());
-    temp.src = "assets/0" + a.toString() + ".png";
-    temp.style.filter = "brightness(1)";
+    x = pos[pos.length - 1][0];
+    y = pos[pos.length - 1][1];
+    if(x >= 1 && x <= n && y >= 1 && y <= m) {
+        const temp = document.getElementById(x.toString() + "." + y.toString());
+        temp.src = "assets/0" + a.toString() + ".png";
+        temp.style.filter = "brightness(1)";
+    }
 }
 
 document.addEventListener('keydown', function(event) {
@@ -72,4 +77,4 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
-let interval = setInterval(update, 500);
+let interval = setInterval(update, 100);
